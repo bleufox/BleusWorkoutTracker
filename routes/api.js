d42e1043fa54da1ce108cd7bd7dee63f0fb32e3d
bleufox <thebleufox8@gmail.com>
2021-10-10 15:07:24 -0700

    Wrote api.js in index.js on accident. Fixed

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const db = require("../models/")
+
+router.get("/workouts", (req, res) => {
+    db.Workout.find(
+        {}).then(workouts => {
+            console.log(workouts)
+            workouts.forEach(workout => {
+                let total = 0
+                workout.exercises.forEach(ex =>
+                    total += ex.duration)
+            })
+            res.json(workouts)
+        })
+})
+
+
