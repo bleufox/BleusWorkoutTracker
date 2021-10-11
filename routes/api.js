936dca6c0473a810a39f7fd817866447b65b89c2
bleufox <thebleufox8@gmail.com>
2021-10-10 17:01:53 -0700

    Added remaining routes

--- routes/api.js
+++ routes/api.js
@@ -18,4 +18,42 @@ router.get("/api/workouts/range", (req, res) => {
 });
 
 
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id, {
+        $push: { exercises: body }
+    }).then(workoutDB => {
+        res.json(workoutDB);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(workoutDB => {
+            res.json(workoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+        .then(workoutDB => {
+            res.json(workoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
 module.exports = router;
