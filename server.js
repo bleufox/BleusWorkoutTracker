c0a2ac5cf3e7cacc23559e0a56d1265f845bcab0
bleufox <thebleufox8@gmail.com>
2021-10-10 17:20:04 -0700

    Removed unnecessary code

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-require('dotenv').config();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
     useNewUrlParser: true,
@@ -16,12 +14,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker
     useCreateUrlParser: true,
     useFindAndModify: false
 }
-).then(console.log('Connected to MongoDB!'))
-.catch((err) => {
-    console.log(err)
-    process.exit(1)
-})
-app.use(router);
+);
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
